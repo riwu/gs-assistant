f6a5c712fe9975ab0dfbbf907c476f929b0179c2
riwu <riwu0730@gmail.com>
2018-07-12 16:04:31 +0800

    add mobile recording

--- mobile/App.js
+++ mobile/App.js
@@ -1,35 +1,173 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image, AppRegistry, TouchableHighlight } from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import Voice from 'react-native-voice';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+export default class VoiceTest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recognized: '',
+      pitch: '',
+      error: '',
+      end: '',
+      started: '',
+      results: [],
+      partialResults: [],
+    };
+    Voice.onSpeechStart = this.onSpeechStart.bind(this);
+    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
+    Voice.onSpeechEnd = this.onSpeechEnd.bind(this);
+    Voice.onSpeechError = this.onSpeechError.bind(this);
+    Voice.onSpeechResults = this.onSpeechResults.bind(this);
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults.bind(this);
+    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged.bind(this);
+  }
 
-type Props = {};
-export default class App extends Component<Props> {
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+
+  onSpeechStart(e) {
+    this.setState({
+      started: '√',
+    });
+  }
+
+  onSpeechRecognized(e) {
+    this.setState({
+      recognized: '√',
+    });
+  }
+
+  onSpeechEnd(e) {
+    this.setState({
+      end: '√',
+    });
+  }
+
+  onSpeechError(e) {
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  }
+
+  onSpeechResults(e) {
+    this.setState({
+      results: e.value,
+    });
+  }
+
+  onSpeechPartialResults(e) {
+    this.setState({
+      partialResults: e.value,
+    });
+  }
+
+  onSpeechVolumeChanged(e) {
+    this.setState({
+      pitch: e.value,
+    });
+  }
+
+  async _startRecognizing(e) {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async _stopRecognizing(e) {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async _cancelRecognizing(e) {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async _destroyRecognizer(e) {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      console.error(e);
+    }
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to GS Assistant!</Text>
+        <Text style={styles.instructions}>Press the button and start speaking.</Text>
+        <Text style={styles.stat}>{`Started: ${this.state.started}`}</Text>
+        <Text style={styles.stat}>{`Recognized: ${this.state.recognized}`}</Text>
+        <Text style={styles.stat}>{`Pitch: ${this.state.pitch}`}</Text>
+        <Text style={styles.stat}>{`Error: ${this.state.error}`}</Text>
+        <Text style={styles.stat}>Results</Text>
+        {this.state.results.map((result, index) => {
+          return (
+            <Text key={`result-${index}`} style={styles.stat}>
+              {result}
+            </Text>
+          );
+        })}
+        <Text style={styles.stat}>Partial Results</Text>
+        {this.state.partialResults.map((result, index) => {
+          return (
+            <Text key={`partial-result-${index}`} style={styles.stat}>
+              {result}
+            </Text>
+          );
+        })}
+        <Text style={styles.stat}>{`End: ${this.state.end}`}</Text>
+        <TouchableHighlight onPress={this._startRecognizing.bind(this)}>
+          <Image style={styles.button} source={require('./button.png')} />
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._stopRecognizing.bind(this)}>
+          <Text style={styles.action}>Stop Recognizing</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._cancelRecognizing.bind(this)}>
+          <Text style={styles.action}>Cancel</Text>
+        </TouchableHighlight>
+        <TouchableHighlight onPress={this._destroyRecognizer.bind(this)}>
+          <Text style={styles.action}>Destroy</Text>
+        </TouchableHighlight>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
@@ -41,9 +179,20 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  action: {
+    textAlign: 'center',
+    color: '#0000FF',
+    marginVertical: 5,
+    fontWeight: 'bold',
+  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  stat: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
   },
 });
