58059502147b2fc61d20fc7a4634977769524f4a
riwu <riwu0730@gmail.com>
2018-07-18 00:43:20 +0800

    update speech recognition on mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,198 +1,87 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, AppRegistry, TouchableHighlight } from 'react-native';
-
+import React from 'react';
+import { StyleSheet, View, Alert, WebView, Dimensions, Button, Platform } from 'react-native';
 import Voice from 'react-native-voice';
+import io from 'socket.io-client';
 
-export default class VoiceTest extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      recognized: '',
-      pitch: '',
-      error: '',
-      end: '',
-      started: '',
-      results: [],
-      partialResults: [],
+const socket = io('https://g.wangriwu.com');
+
+socket.on('connect', () => {
+  console.log('Socket connection established');
+});
+
+socket.on('disconnect', () => {
+  console.log('Socket connection lost');
+});
+
+const { width } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 20 : 0,
+  },
+});
+
+const startTranscription = () => Voice.start('en-US');
+
+export default class App extends React.Component {
+  state = {
+    started: false,
+  };
+  componentDidMount() {
+    Voice.onSpeechStart = () => console.log('started');
+    Voice.onSpeechRecognized = () => console.log('recognized');
+    Voice.onSpeechEnd = () => {
+      console.log('ended');
+      if (this.value) {
+        socket.send(this.value, true);
+      }
+      startTranscription()
+        .then(() => console.log('restarted'))
+        .catch(e => Alert.alert('Failed to start recording', e.message));
     };
-    Voice.onSpeechStart = this.onSpeechStart.bind(this);
-    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this);
-    Voice.onSpeechEnd = this.onSpeechEnd.bind(this);
-    Voice.onSpeechError = this.onSpeechError.bind(this);
-    Voice.onSpeechResults = this.onSpeechResults.bind(this);
-    Voice.onSpeechPartialResults = this.onSpeechPartialResults.bind(this);
-    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged.bind(this);
+    Voice.onSpeechPartialResults = (e) => {
+      console.log('result', e);
+      this.value = e.value;
+      if (e.value) {
+        socket.send(e.value, false);
+      }
+    };
+    Voice.onSpeechResults = e => console.log('final result', e);
+    Voice.onSpeechError = (e) => {
+      console.log('err', e);
+      if (!e.error.message.includes('No speech input')) {
+        Alert.alert('Error', e.error.message);
+      }
+      startTranscription()
+        .then(() => console.log('restarted'))
+        .catch(err => Alert.alert('Failed to start recording', err.message));
+    };
   }
 
   componentWillUnmount() {
     Voice.destroy().then(Voice.removeAllListeners);
   }
 
-  onSpeechStart(e) {
-    this.setState({
-      started: '√',
-    });
-  }
-
-  onSpeechRecognized(e) {
-    this.setState({
-      recognized: '√',
-    });
-  }
-
-  onSpeechEnd(e) {
-    this.setState({
-      end: '√',
-    });
-  }
-
-  onSpeechError(e) {
-    this.setState({
-      error: JSON.stringify(e.error),
-    });
-  }
-
-  onSpeechResults(e) {
-    this.setState({
-      results: e.value,
-    });
-  }
-
-  onSpeechPartialResults(e) {
-    this.setState({
-      partialResults: e.value,
-    });
-  }
-
-  onSpeechVolumeChanged(e) {
-    this.setState({
-      pitch: e.value,
-    });
-  }
-
-  async _startRecognizing(e) {
-    this.setState({
-      recognized: '',
-      pitch: '',
-      error: '',
-      started: '',
-      results: [],
-      partialResults: [],
-      end: '',
-    });
-    try {
-      await Voice.start('en-US');
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async _stopRecognizing(e) {
-    try {
-      await Voice.stop();
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async _cancelRecognizing(e) {
-    try {
-      await Voice.cancel();
-    } catch (e) {
-      console.error(e);
-    }
-  }
-
-  async _destroyRecognizer(e) {
-    try {
-      await Voice.destroy();
-    } catch (e) {
-      console.error(e);
-    }
-    this.setState({
-      recognized: '',
-      pitch: '',
-      error: '',
-      started: '',
-      results: [],
-      partialResults: [],
-      end: '',
-    });
-  }
-
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to GS Assistant!</Text>
-        <Text style={styles.instructions}>Press the button and start speaking.</Text>
-        <Text style={styles.stat}>{`Started: ${this.state.started}`}</Text>
-        <Text style={styles.stat}>{`Recognized: ${this.state.recognized}`}</Text>
-        <Text style={styles.stat}>{`Pitch: ${this.state.pitch}`}</Text>
-        <Text style={styles.stat}>{`Error: ${this.state.error}`}</Text>
-        <Text style={styles.stat}>Results</Text>
-        {this.state.results.map((result, index) => {
-          return (
-            <Text key={`result-${index}`} style={styles.stat}>
-              {result}
-            </Text>
-          );
-        })}
-        <Text style={styles.stat}>Partial Results</Text>
-        {this.state.partialResults.map((result, index) => {
-          return (
-            <Text key={`partial-result-${index}`} style={styles.stat}>
-              {result}
-            </Text>
-          );
-        })}
-        <Text style={styles.stat}>{`End: ${this.state.end}`}</Text>
-        <TouchableHighlight onPress={this._startRecognizing.bind(this)}>
-          <Image style={styles.button} source={require('./button.png')} />
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._stopRecognizing.bind(this)}>
-          <Text style={styles.action}>Stop Recognizing</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._cancelRecognizing.bind(this)}>
-          <Text style={styles.action}>Cancel</Text>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={this._destroyRecognizer.bind(this)}>
-          <Text style={styles.action}>Destroy</Text>
-        </TouchableHighlight>
+        <Button
+          onPress={() => {
+            if (this.state.started) {
+              Voice.stop()
+                .then(() => this.setState({ started: false }))
+                .catch(e => Alert.alert('Failed to stop recording', e.message));
+            } else {
+              startTranscription()
+                .then(() => this.setState({ started: true }))
+                .catch(e => Alert.alert('Failed to start recording', e.message));
+            }
+          }}
+          title={this.state.started ? 'Stop recording' : 'Start recording'}
+        />
+        <WebView source={{ uri: 'https://riwu.github.io/gs-assistant' }} style={{ width }} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  button: {
-    width: 50,
-    height: 50,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  action: {
-    textAlign: 'center',
-    color: '#0000FF',
-    marginVertical: 5,
-    fontWeight: 'bold',
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  stat: {
-    textAlign: 'center',
-    color: '#B0171F',
-    marginBottom: 1,
-  },
-});
