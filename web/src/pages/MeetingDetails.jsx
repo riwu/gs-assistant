38ed834d7299dfd1dbfd0be430ac3a3032e22738
riwu <riwu0730@gmail.com>
2018-07-20 00:05:56 +0800

    update meeting view name order

--- web/src/pages/MeetingDetails.jsx
+++ web/src/pages/MeetingDetails.jsx
@@ -5,7 +5,7 @@ const MeetingDetails = () => (
     <b>Title:</b> Singapore Technology Internship Group Project Presentation 2018<br />
     <b>Date:</b> Thu, 5 July 3pm - 4.15pm<br />
     <b>Location:</b> Mapletree Anson Level 14 MP1<br />
-    <b>Members:</b> Ding Jin, Sheng Yu, Grace Ting, Jeremy Ow, Wang Riwu
+    <b>Members:</b> Ding Jin, Jeremy Ow, Sheng Yu, Grace Ting, Wang Riwu
   </React.Fragment>
 );
 
